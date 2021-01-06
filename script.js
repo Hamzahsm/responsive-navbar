585cf1b494ce7f666c4e848b6087d762046ff963
Hamzahsm <hamzahsaifulmadjid@gmail.com>
2021-01-06 11:24:50 +0700

    adding file index, script and style

---
+++ script.js
@@ -0,0 +1,4 @@
+const menuToggle = document.querySelector('.menu-toggle input').addEventListener('click', function(){
+    nav.classList.toggle('slide'); 
+});
+const nav = document.querySelector('nav ul');
